d9744d984b1a85e6e542be6ceb50cebe53a3ce9e
Anna Korolkova <annie2505@mail.ru>
2022-03-25 13:06:31 +0300

    исправить замечания после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,37 @@
 let popup = document.querySelector('.popup');
-let openPopup = document.querySelector('.profile__edit-button');
-let closePopup = popup.querySelector('.popup__close-button');
+let openedPopup = document.querySelector('.profile__edit-button');
+let closedPopup = popup.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input_form_name');
-let jobInput = formElement.querySelector('.popup__input_form_job');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_job');
 
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
+function openPopup() {
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
-popup.addEventListener('click', function(event) {
-  if (event.target === event.currentTarget) {
-    togglePopup();
-  }
-})
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
 
-openPopup.addEventListener('click', togglePopup);
-closePopup.addEventListener('click', togglePopup);
+/* popup.addEventListener('click', function(event) {
+  if (event.target === event.currentTarget) {
+    closePopup();
+  }
+}) */
+
+openedPopup.addEventListener('click', openPopup);
+closedPopup.addEventListener('click', closePopup);
 
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    togglePopup();
+    closePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
